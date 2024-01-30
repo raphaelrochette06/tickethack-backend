d0812858be81dca3ef03e918613ad7731ddf9232
Viviane2111 <schommersv06@gmail.com>
2024-01-30 11:04:39 +0100

    creating the connection

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://Arrghe:Fi8e0Q2EWMA7KPGk@cluster0.wvfgucy.mongodb.net/weatherapp';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
