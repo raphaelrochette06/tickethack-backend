d5daa69ecf700594ffe7182e226c156623070fce
Rochette Raphaël <rochette.raph@gmail.com>
2024-01-31 10:42:20 +0100

    fix post trips route

--- routes/trips.js
+++ routes/trips.js
@@ -7,26 +7,25 @@ const Trip = require("../models/trips");
 
 // Récupérer tous les voyages
 router.get("/", (req, res) => {
-    Trip.find().then(data => {
-        res.json({ trips: data })
-    });
+  Trip.find().then((data) => {
+    res.json({ trips: data });
+  });
 });
 
 router.post("/findTrips", (req, res) => {
-    const today = moment()
-    Trip.find({
-        departure: req.body.departure,
-        arrival: req.body.arrival,
-        date: { $gte: today.toDate(),
-                $lte: moment(today).endOf('day').toDate()}
-    }).then((data) => {
-        console.log(data);
-        if (data.length === 0) {
-            res.json({ result: false, error: "No trips matching"});
-        }
-        res.json({ result: true, allTrips: data });
-    });
+  const today = moment();
+  Trip.find({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    date: { $gte: today.toDate(), $lte: moment(today).endOf("day").toDate() },
+  }).then((data) => {
+    console.log(data);
+    if (data.length === 0) {
+      res.json({ result: false, error: "No trips matching" });
+    } else {
+      res.json({ result: true, allTrips: data });
+    }
+  });
 });
-
 
 module.exports = router;
